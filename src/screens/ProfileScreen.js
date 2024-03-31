83c3b46ec718718300e3434b798b8984b54e35c7
Fabian Roh <fabiankmroh@gmail.com>
2024-03-31 02:55:48 -0700

    Profile Picture & Text Elements

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import { View, Text, Image, Button, StyleSheet } from "react-native";
 
 const ProfileScreen = ({navigation}) => {
     const goHome = () => {
@@ -7,11 +7,50 @@ const ProfileScreen = ({navigation}) => {
     }
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Andrew</Text>
-      <Button title = "Home" onPress={goHome} />
+    <View style={styles.profileContainer}>
+      <Image 
+        source = {require('../media/ksea-logo.jpg')}
+        style = {styles.KSEAlogo}
+        resizeMode = "contain"
+      />
+      <Image 
+        source = {require('../media/namwook.jpg')}
+        style = {styles.profileImage}
+        resizeMode = "contain"
+      />
+      <Text style = {styles.name}>Andrew Namwook Lee</Text>
+      <Text style = {styles.email}>sexypepe@berkeley.edu</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  KSEAlogo: {
+    width: 100,
+    height: 100,
+  },
+  profileContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: '#000',
+  },
+  profileImage: {
+    width: 200,
+    height: 200, 
+  },
+  name: {
+    fontSize: 20,
+    marginTop: 15, 
+    marginBottom: 10, 
+    color: 'white',
+  },
+  email: {
+    fontSize: 15,
+    marginTop: 5,
+    marginBottom: 10,
+    color: 'white',
+  }
+})
 
 export default ProfileScreen;
