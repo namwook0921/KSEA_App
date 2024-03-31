f3f3d5cbacdaa7f397c92779a805d8fba03c2414
Fabian Roh <fabiankmroh@gmail.com>
2024-03-30 21:34:40 -0700

    Login Screen Build: Keyboard Blocking Screen Error

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,6 +1,6 @@
 // LoginScreen.js
 import React, { useState } from "react";
-import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import { View, Text, Image, TextInput, Button, StyleSheet } from "react-native";
 
 const LoginScreen = ({ navigation }) => {
   const [email, setEmail] = useState("");
@@ -20,15 +20,20 @@ const LoginScreen = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
+      <Image 
+        source={require('../media/ksea-logo.jpg')}
+        style = {styles.imageStyle}
+        resizeMode="contain"
+      />
       <Text style={styles.title}>Login</Text>
       <TextInput
-        style={styles.input}
+        style={styles.TextInput}
         placeholder="Email"
         value={email}
         onChangeText={setEmail}
       />
       <TextInput
-        style={styles.input}
+        style={styles.TextInput}
         placeholder="Password"
         value={password}
         onChangeText={setPassword}
@@ -43,18 +48,24 @@ const LoginScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: "center", // Center the Image Vertically
+    alignItems: "center", // Center the Image Horizontally
+    backgroundColor: '#000', // Background: Black
+  },
+  imageStyle: {
+    width: 200,
+    height: 200, 
   },
   title: {
     fontSize: 24,
     marginBottom: 20,
   },
-  input: {
+  TextInput: {
     width: "80%",
     marginBottom: 10,
     padding: 10,
     borderWidth: 1,
+    color: 'white',
     borderColor: "#ccc",
     borderRadius: 5,
   },
