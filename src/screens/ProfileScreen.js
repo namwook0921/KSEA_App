5fb99bf00466424af3745623727c2077043c35cd
Fabian Roh <fabiankmroh@gmail.com>
2024-04-03 23:36:59 -0700

    ProfileScreen: Container Readjustment

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -7,48 +7,69 @@ const ProfileScreen = ({navigation}) => {
     }
 
   return (
-    <View style={styles.profileContainer}>
-      <Image 
-        source = {require('../media/ksea-logo.jpg')}
-        style = {styles.KSEAlogo}
-        resizeMode = "contain"
-      />
-      <Image 
-        source = {require('../media/namwook.jpg')}
-        style = {styles.profileImage}
-        resizeMode = "contain"
-      />
-      <Text style = {styles.name}>Andrew Namwook Lee</Text>
-      <Text style = {styles.email}>sexypepe@berkeley.edu</Text>
+    <View style = {styles.EntireContainer}>
+    <View style = {styles.BackContainer}>
+      <Button title = "Back" onPress = {goHome} />
     </View>
+    <View style = {styles.MainContainer}>
+      <View style = {styles.KSEAContainer}>
+        <Image
+          source = {require('../media/ksea-logo.jpg')}
+          style = {styles.KSEALogo}
+          resizeMode = "contain"
+        />
+      </View>
+      <Text style = {styles.name}>Andrew Namwook Lee</Text>
+      <Text style = {styles.name}>이남욱</Text>
+      <Text style = {styles.mail}>sexypepe@berkeley.edu</Text>
+
+    </View>
+  </View>
   );
 };
 
 const styles = StyleSheet.create({
-  KSEAlogo: {
-    width: 100,
-    height: 100,
-  },
-  profileContainer: {
+  EntireContainer: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: '#000',
   },
-  profileImage: {
-    width: 200,
-    height: 200, 
+  BackContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: 100, 
+    height: 100,
+    backgroundColor: '#000'
+  },
+  MainContainer: {
+    flex: 1, 
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000',
+    borderWidth: 2,
+    borderColor: '#FFF'
+  },
+  KSEAContainer: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'stretch',
+  },
+  KSEALogo: {
+    width: 100,
+    height: 100, 
   },
   name: {
-    fontSize: 20,
-    marginTop: 15, 
+    fontSize: 25,
+    marginTop: 10,
     marginBottom: 10, 
     color: 'white',
   },
-  email: {
+  mail: {
     fontSize: 15,
     marginTop: 5,
-    marginBottom: 10,
+    marginBottom: 15,
     color: 'white',
   }
 })
