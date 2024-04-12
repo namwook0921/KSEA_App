635cb4ff97a831f4850b71451a117f8df75fcc0e
Jaewon Hur <ryanhur3080@gmail.com>
2024-04-12 11:58:10 -0700

    initialized branch for setting up Firebase

--- src/utils/FileUtils.js
+++ src/utils/FileUtils.js
@@ -63,41 +63,6 @@ export async function loadFile(filename) {
   }
 }
 
-
-// const fs = require('fs');
-
-// // Function to save login information to a text file
-// function saveFile(filename, loginInfo) {
-//   const { username, password } = loginInfo;
-//   const data = `${username}:${password}\n`;
-
-//   try {
-//     fs.appendFileSync(filename, data);
-//     console.log('Login information saved to file successfully');
-//   } catch (error) {
-//     console.error('Error saving login information:', error);
-//   }
-// }
-
-// // Function to load login information from a text file
-// function loadFile(filename) {
-//   try {
-//     const data = fs.readFileSync(filename, 'utf8');
-//     const lines = data.split('\n').filter(line => line.trim() !== '');
-//     const loginDatabase = {};
-
-//     lines.forEach(line => {
-//       const [username, password] = line.split(':');
-//       loginDatabase[username] = password;
-//     });
-
-//     return loginDatabase;
-//   } catch (error) {
-//     console.error('Error loading login information:', error);
-//     return null;
-//   }
-// }
-
 // // Example usage:
 // const filename = 'loginDatabase.txt';
 // const newLoginInfo = { username: 'user1', password: 'password1' };
